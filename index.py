d5b5a8b10ccb088368a006e9b3605707b3a5c94c
Elio Goettelmann <elio@goettelmann.fr>
2017-11-05 21:17:22 +0100

    Added login filter

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, session, abort
 
 from backend.dbconnector.manager import EntityManager
 from backend.modules.depynject import Depynject
@@ -23,6 +23,15 @@ em.init()
 @app.route("/")
 def serve_page():
     return app.send_static_file("index.html")
+
+
+@app.before_request
+def before_request():
+    if request.path.startswith(api.prefix) and request.endpoint.startswith('SessionController'):
+        if 'logged_user_id' not in session:
+            abort(403)
+        else:
+            print('user ' + str(session['logged_user_id']))
 
 
 api.register(AccountController)
