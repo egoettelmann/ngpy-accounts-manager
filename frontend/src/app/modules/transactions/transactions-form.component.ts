6bd77dd8eb8a6b9424f18760e897725f659577ac
Elio Goettelmann <elio@goettelmann.fr>
2017-11-14 18:58:07 +0100

    Added transactions-form component

---
+++ frontend/src/app/modules/transactions/transactions-form.component.ts
@@ -0,0 +1,42 @@
+import {Component, Input} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-transactions-form',
+  templateUrl: './transactions-form.component.html'
+})
+export class TransactionsFormComponent {
+
+  @Input() buttonLabel = 'i18n.transactions.form.button.add';
+
+  form: FormGroup;
+
+  constructor() {
+    this.form = new FormGroup(
+      {
+        reference: new FormControl(
+          null,
+          Validators.required
+        ),
+        description: new FormControl(null),
+        dateValue: new FormControl(
+          null,
+          Validators.required
+        ),
+        amount: new FormControl(
+          null,
+          Validators.required
+        )
+      }
+    );
+  }
+
+  getControlList() {
+    return Object.values(this.form.controls);
+  }
+
+  saveTransaction() {
+    console.log('submit');
+  }
+
+}
--- frontend/src/app/modules/transactions/transactions.module.ts
+++ frontend/src/app/modules/transactions/transactions.module.ts
@@ -3,19 +3,24 @@ import {TransactionsTableComponent} from './transactions-table.component';
 import {TransactionsService} from './transactions.service';
 import {SharedModule} from '../shared/shared.module';
 import {StatisticsModule} from '../statistics/statistics.module';
+import {TransactionsFormComponent} from './transactions-form.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    TransactionsTableComponent
+    TransactionsTableComponent,
+    TransactionsFormComponent
   ],
   exports: [
     TransactionsTableComponent,
+    TransactionsFormComponent
   ],
   providers: [
     TransactionsService
   ],
   imports: [
     StatisticsModule,
+    ReactiveFormsModule,
     SharedModule
   ]
 })
