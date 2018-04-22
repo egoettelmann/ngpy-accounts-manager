55dead4013e77085bf1ab311132d2051290be1aa
Elio Goettelmann <elio.goettelmann@arhs-cube.com>
2018-04-22 15:42:21 +0200

    Changed dashboard view (added pie chart)

--- frontend/src/app/views/dashboard/dashboard-view.component.ts
+++ frontend/src/app/views/dashboard/dashboard-view.component.ts
@@ -1,16 +1,20 @@
-import {Component, OnInit} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {AccountsService} from "../../modules/accounts/accounts.service";
+import { Component, OnInit } from '@angular/core';
+import { AccountsService } from '../../modules/accounts/accounts.service';
+import { DecimalPipe } from '@angular/common';
+import { Account } from '../../modules/accounts/account';
 
 @Component({
   templateUrl: './dashboard-view.component.html'
 })
 export class DashboardViewComponent implements OnInit {
 
-  public accounts: any[];
+  public accounts: Account[];
   public total: number;
+  public graphOptions: any;
 
-  constructor(private accountsService: AccountsService) {}
+  constructor(private accountsService: AccountsService,
+              private decimalPipe: DecimalPipe
+  ) {}
 
   ngOnInit(): void {
     this.accountsService.getAccounts().subscribe(data => {
@@ -19,7 +23,36 @@ export class DashboardViewComponent implements OnInit {
       for (const a of this.accounts) {
         this.total += a.total;
       }
+      this.graphOptions = this.buildChartOptions(data, this.total);
     });
+  }
+
+  buildChartOptions(data: Account[], total: number) {
+    const that = this;
+    const options = {
+      chart: {
+        type: 'pie'
+      },
+      tooltip: {
+        formatter: function () {
+          return '<b>' + this.point.name + '</b><br/>'
+            + '<b>' + that.decimalPipe.transform(this.y, '1.2-2') + ' €</b>'
+            + ' (' + that.decimalPipe.transform(this.percentage, '1.2-2') + '%)';
+        }
+      },
+      series: [{
+        data: [],
+        showInLegend: false
+      }]
+    };
+    for (const d of data) {
+      options.series[0].data.push({
+        name: d.description,
+        color: d.color,
+        y: d.total
+      });
+    }
+    return options;
   }
 
 }
