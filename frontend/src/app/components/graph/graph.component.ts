aa1e0188469d380dfaee042d375ee7ec48c0081a
Elio Goettelmann <elio.goettelmann@arhs-cube.com>
2018-04-23 21:38:56 +0200

    Added title to graph component and updated all translations

--- frontend/src/app/components/graph/graph.component.ts
+++ frontend/src/app/components/graph/graph.component.ts
@@ -1,15 +1,18 @@
-import {Component, ElementRef, Input, OnChanges, SimpleChanges} from '@angular/core';
+import { Component, ElementRef, Input, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
 import * as Highcharts from 'highcharts';
 import * as _ from 'lodash';
 
 @Component({
   selector: 'app-chart',
-  template: '&nbsp;',
-  styleUrls: ['./graph.component.css']
+  templateUrl: './graph.component.html',
+  styleUrls: ['./graph.component.scss']
 })
 export class GraphComponent implements OnChanges {
 
+  @Input() title: string;
   @Input() options: any;
+
+  @ViewChild("graphContainer") graphContainer: ElementRef;
 
   private defaultOptions = {
     chart: {
@@ -32,12 +35,10 @@ export class GraphComponent implements OnChanges {
     }
   };
 
-  constructor(private element: ElementRef) {}
-
   ngOnChanges(changes: SimpleChanges): void {
     if (changes.options != null && this.options) {
       const opts = _.merge({}, this.defaultOptions, this.options);
-      Highcharts.chart(this.element.nativeElement, opts);
+      Highcharts.chart(this.graphContainer.nativeElement, opts);
     }
   }
 
