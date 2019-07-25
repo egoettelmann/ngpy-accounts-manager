611bd48770c57730ca2cbfbbadb198f3fa214e89
elio.goettelmann <elio.goettelmann@gmail.com>
2019-07-26 00:05:17 +0200

    Fixed some styling issues + improved responsiveness

--- frontend/src/app/shared/components/accounts/card/account-card.component.ts
+++ frontend/src/app/shared/components/accounts/card/account-card.component.ts
@@ -3,7 +3,8 @@ import { Account } from '../../../../core/models/api.models';
 
 @Component({
   selector: 'app-account-card',
-  templateUrl: './account-card.component.html'
+  templateUrl: './account-card.component.html',
+  styleUrls: ['./account-card.component.scss']
 })
 export class AccountCardComponent {
 
