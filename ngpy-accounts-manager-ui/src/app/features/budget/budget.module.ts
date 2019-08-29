81084f5e2753e8014277751290378626edb47cb2
elio.goettelmann <elio.goettelmann@gmail.com>
2019-08-29 11:56:33 +0200

    Added budget card component

--- ngpy-accounts-manager-ui/src/app/features/budget/budget.module.ts
+++ ngpy-accounts-manager-ui/src/app/features/budget/budget.module.ts
@@ -5,13 +5,15 @@ import { BudgetRoutingModule } from './budget-routing.module';
 import { BudgetChartComponent } from './components/budget-chart/budget-chart.component';
 import { BudgetListComponent } from './views/budget-list/budget-list.component';
 import { BudgetDetailsComponent } from './views/budget-details/budget-details.component';
+import { BudgetCardComponent } from './components/budget-card/budget-card.component';
 
 @NgModule({
   declarations: [
     BudgetComponent,
     BudgetListComponent,
     BudgetDetailsComponent,
-    BudgetChartComponent
+    BudgetChartComponent,
+    BudgetCardComponent
   ],
   imports: [
     SharedModule,
---
+++ ngpy-accounts-manager-ui/src/app/features/budget/components/budget-card/budget-card.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Budget } from '../../../../core/models/api.models';
+
+@Component({
+  selector: 'app-budget-card',
+  templateUrl: './budget-card.component.html',
+  styleUrls: ['./budget-card.component.scss']
+})
+export class BudgetCardComponent {
+
+  @Input() budget: Budget;
+
+}
