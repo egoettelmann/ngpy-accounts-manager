9207d1213a796b8a87a83f313d64a3b0aefe2b6f
elio.goettelmann <elio.goettelmann@gmail.com>
2019-12-30 17:34:53 +0100

    Added event on upload rest service

--- ngpy-accounts-manager-ui/src/app/core/services/rest/upload-rest.service.ts
+++ ngpy-accounts-manager-ui/src/app/core/services/rest/upload-rest.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { EventBusService } from '../event-bus.service';
+import { tap } from 'rxjs/operators';
 
 /**
  * The upload rest service.
@@ -12,8 +14,12 @@ export class UploadRestService {
    * Instantiates the service.
    *
    * @param http the HTTP client
+   * @param eventBusService the event bus service
    */
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,
+              private eventBusService: EventBusService
+  ) {
+  }
 
   /**
    * Uploads a file containing transactions.
@@ -24,7 +30,9 @@ export class UploadRestService {
   uploadFile(fileContent: any, fileName: string): Observable<any> {
     const formData = new FormData();
     formData.append('file', fileContent, fileName);
-    return this.http.post<any>('/rest/transactions/upload-file', formData);
+    return this.http.post<any>('/rest/transactions/upload-file', formData).pipe(
+      tap(() => this.eventBusService.publish('transactions.upload', fileName))
+    );
   }
 
 }
